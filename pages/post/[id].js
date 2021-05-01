28ac92d8308b69b6c3f43d5c60e70faafc9dd262
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-01 07:38:50 +0300

    - updated

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,7 +1,8 @@
 import {useRouter} from "next/router";
 
 export default function Post() {
-    useRouter()
+    const router = useRouter()
+    console.log(router)
     return <>
         <h1>Post</h1>
     </>
