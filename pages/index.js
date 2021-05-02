8f3ee526948cf37a2e5feffe1c1ff02d1e7f0e2c
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-02 06:27:12 +0300

    - updated

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ export default function Index() {
     return (
         <>
             <Head><title>Next title index hello</title></Head>
+            <meta name='keywords' content='key words'/>
             <h1>Hello Next.JS!</h1>
             <p>lorem ipsum dolores</p>
             <p><Link href={'/about'}><a>About</a></Link></p>
