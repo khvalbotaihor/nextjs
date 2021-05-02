bb8882928f08f9d2ce0ea2eff67852f5a7aee000
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-02 05:50:13 +0300

    - updated

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
 export default function Index() {
-    return <h1>Hello Next.JS!</h1>
+    return <>
+        <h1>Hello Next.JS!</h1>
+        <p>lorem ipsum dolores</p>
+        <p><a href="/about">About</a></p>
+        <p><a href="/posts">Posts</a></p>
+        </>
+    
 }
