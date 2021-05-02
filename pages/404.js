eca0524c4bbb6a81bb21a8b120352fb84ec1deb0
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-02 11:31:22 +0300

    - updated

--- pages/404.js
+++ pages/404.js
@@ -2,7 +2,7 @@ export default function ErrorPage() {
     return (
         <>
             <h1>Error 404</h1>
-            <p>Please <Link href='/'>go back</Link> to safety</p>
+            <p>Please <Link href='/'><a>go back</a></Link> to safety</p>
         </>
     )
 }
