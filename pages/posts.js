3953fd1092c26f7ef757e1a5e5ee9ecd80483e48
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-02 06:25:00 +0300

    - updated

--- pages/posts.js
+++ pages/posts.js
@@ -4,6 +4,7 @@ import Head from 'next/head'
 export default function Posts() {
     return (
         <>
+            <Head><title>Posts page | Next course</title></Head>
             <h1>Posts Page</h1>
             <p>lorem ipsum</p>
         </>
