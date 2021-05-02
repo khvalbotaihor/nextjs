074969ede745fd2259dac92ccd100ec8ee80075e
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-02 06:04:29 +0300

    - updated

--- pages/about/index.js
+++ pages/about/index.js
@@ -9,7 +9,7 @@ export default function Index() {
     return (
         <React.Fragment>
             <h1>About Page</h1>
-            <button onClick={}>Go back to home</button>
+            <button onClick={linkClickHandler}>Go back to home</button>
         </React.Fragment>
     )
 }
