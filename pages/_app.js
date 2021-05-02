2bed2878dee092a714d681ec77af87a0ef5799d4
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-02 08:10:45 +0300

    - updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({Component, pageProps}) {
     return (
         <>
             <Component {...pageProps}/>
             <style jsx>{`
             body {
-            
+                font-family: 'Roboto', sans-serif;
             }
             `}</style>
         </>
