47fa9781ccd4bb281eb77681a23a80ba109f65af
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-02 06:46:25 +0300

    - updated

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Router from 'next/router'
+import {MainLayout} from "../../components/MainLayout";
 
 export default function Index() {
 
@@ -8,10 +9,10 @@ export default function Index() {
     }
 
     return (
-        <React.Fragment>
+        <MainLayout>
             <h1>About Page</h1>
             <button onClick={linkClickHandler}>Go back to home</button>
             <button onClick={() => Router.push('/posts')}>Go to posts</button>
-        </React.Fragment>
+        </MainLayout>
     )
 }
