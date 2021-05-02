a0b338175e7983edb4d4a985a98109439624a973
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-02 06:20:07 +0300

    - updated

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Link from 'next/link'
 
 export default function Index() {
-    return <>
-        <h1>Hello Next.JS!</h1>
-        <p>lorem ipsum dolores</p>
-        <p><Link href={'/about'}><a>About</a></Link></p>
-        <p><Link href='/posts'><a>Posts</a></Link></p>
-    </>
+    return (
+        <>
+            <h1>Hello Next.JS!</h1>
+            <p>lorem ipsum dolores</p>
+            <p><Link href={'/about'}><a>About</a></Link></p>
+            <p><Link href='/posts'><a>Posts</a></Link></p>
+        </>
+    )
 }
