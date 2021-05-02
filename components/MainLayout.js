5d883d30fa28056c205fbbae2f6df1427649ecd5
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-02 06:44:18 +0300

    - updated

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -2,7 +2,7 @@ export function MainLayout({children}) {
     return (
         <>
             <nav>
-
+                <h1>Navigation</h1>
             </nav>
             <main>
                 {children}
