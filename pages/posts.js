d6fd9b5d5792c65f6f76ce36aeb5386bed81c8ca
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-02 07:39:28 +0300

    - updated

--- pages/posts.js
+++ pages/posts.js
@@ -5,7 +5,6 @@ import {MainLayout} from "../components/MainLayout";
 export default function Posts() {
     return (
         <MainLayout title='Posts page'>
-            <Head><title>Posts page | Next course</title></Head>
             <h1>Posts Page</h1>
             <p>lorem ipsum</p>
         </MainLayout>
