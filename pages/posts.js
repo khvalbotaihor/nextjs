de3632a3042f411c056c74afade98dabb3e83a43
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-03 15:35:20 +0300

    - updated

--- pages/posts.js
+++ pages/posts.js
@@ -1,18 +1,21 @@
 import {MainLayout} from "../components/MainLayout";
 import {useState, useEffect} from 'react';
 import Link from "next/link";
+import {useRouter} from "next/router";
 
 export default function Posts({posts}) {
 
-/*    const [posts, setPosts] = useState([])
+    const [posts, setPosts] = useState([])
     useEffect(() => {
         async function load() {
           const response = await fetch("http://localhost:4200/posts");
           const json = await response.json();
           setPosts(json)
         }
-        load();
-    },[])*/
+        if (!posts ){
+            load()
+        }
+    },[])
 
     return (
         <MainLayout title={'Posts page'}>
