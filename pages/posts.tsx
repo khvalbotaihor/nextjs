327bf37a50c06686bfe8a4bc41e9be43a141bfe7
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-03 17:14:00 +0300

    - updated

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -52,7 +52,7 @@ Posts.getInitialProps = async ({req}: NextPageContext) => {
         return {posts: null}
     }
 
-    const response = await fetch("http://localhost:4200/posts");
+    const response = await fetch(`${process.env.API_URL}/posts`);
     const posts: MyPost = await response.json();
 
     return {
