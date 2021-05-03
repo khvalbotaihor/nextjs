5eaad0780f1a4299134d486ec4cf9f1658716ba3
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-03 08:31:50 +0300

    - updated

--- pages/404.js
+++ pages/404.js
@@ -5,7 +5,7 @@ import classes from '../styles/error.module.css'
 export default function ErrorPage() {
     return (
         <MainLayout>
-            <h1 className={classes}>Error 404</h1>
+            <h1 className={classes.error}>Error 404</h1>
             <p>Please <Link href='/'><a>go back</a></Link> to safety</p>
         </MainLayout>
     )
