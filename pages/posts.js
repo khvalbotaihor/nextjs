40b7c0b67d9ebfaf8c60005ed3cb41dc61d31cc8
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-03 09:25:05 +0300

    - updated

--- pages/posts.js
+++ pages/posts.js
@@ -6,7 +6,7 @@ export default function Posts() {
     const [posts, setPosts] = useState([])
     useEffect(() => {
         async function load() {
-           await fetch("http://localhost:4200/posts")
+          const response = await fetch("http://localhost:4200/posts")
         }
     },[])
 
