50ceed7edfb791e64b8654d17515c99764c8d5f7
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-03 10:08:52 +0300

    - updated

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,9 +1,11 @@
 import {useRouter} from "next/router";
 import {MainLayout} from "../../components/MainLayout";
 
-export default function Post() {
+export default function Post({posts}) {
     const router = useRouter()
     return <MainLayout title={'Post page'}>
         <h1>Post {router.query.id}</h1>
+        <h2>{posts.title}</h2>
+        <p>{posts.body}</p>
     </MainLayout>
 }
