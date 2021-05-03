36d54489e4d4f76163c8224c2c1cab0867759b22
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-03 17:02:50 +0300

    - updated

--- pages/api/echo.tsx
+++ pages/api/echo.tsx
@@ -1,4 +1,6 @@
-export default function echo(req, res) {
+import {NextApiRequest, NextApiResponse} from "next";
+
+export default function echo(req: NextApiRequest, res: NextApiResponse) {
     res.statusCode = 200
     res.setHeader('Content-type','application/json')
     res.end(JSON.stringify({
