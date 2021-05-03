d59953452406dbcd46134e63cafe8317808e8aa6
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-03 08:56:15 +0300

    - updated

--- pages/posts.js
+++ pages/posts.js
@@ -1,14 +1,22 @@
 import {MainLayout} from "../components/MainLayout";
-import { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
 
 export default function Posts() {
 
     const [posts, setPosts] = useState([])
+    useEffect(() => {
+        async function load() {
+            fetch("http://localhost:4200/posts")
+        }
+    },[])
 
     return (
         <MainLayout title={'Posts page'}>
             <h1>Posts Page</h1>
             <p>lorem ipsum</p>
+            <ul>
+
+            </ul>
         </MainLayout>
     )
 }
