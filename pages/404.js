4b7f5ae3d6ed773a46e5cd52c3b710a453c66f31
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-03 08:31:19 +0300

    - updated

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import {MainLayout} from "../components/MainLayout";
+import classes from '../styles/error.module.css'
 
 export default function ErrorPage() {
     return (
         <MainLayout>
-            <h1>Error 404</h1>
+            <h1 className={classes}>Error 404</h1>
             <p>Please <Link href='/'><a>go back</a></Link> to safety</p>
         </MainLayout>
     )
