67fc19cbac7b4c260d13abd39ff914a3ab68b4ec
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-03 16:13:53 +0300

    - updated

--- pages/about/index.js
+++ pages/about/index.js
@@ -10,7 +10,7 @@ export default function About( {title} ) {
 
     return (
         <MainLayout title={'About Page'}>
-            <h1>{title}</h1>
+            <h1>{JSON.stringify(title)}</h1>
             <button onClick={linkClickHandler}>Go back to home</button>
             <button onClick={() => Router.push('/posts')}>Go to posts</button>
         </MainLayout>
@@ -22,6 +22,6 @@ About.getInitialProps = async () => {
     const data = await response.json()
 
     return {
-        title: data
+        title: data.title
     }
 }
