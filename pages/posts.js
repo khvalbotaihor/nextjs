b95542d1bb1844f3c64579b2da20ca7eca049297
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-03 08:54:16 +0300

    - updated

--- pages/posts.js
+++ pages/posts.js
@@ -2,6 +2,9 @@ import {MainLayout} from "../components/MainLayout";
 import { useState, useEffect } from 'react';
 
 export default function Posts() {
+
+    const [posts, setPosts] = useState([])
+
     return (
         <MainLayout title={'Posts page'}>
             <h1>Posts Page</h1>
