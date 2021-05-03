fedfa01cf4deeb16a2ac355ff0390ebaa16e458e
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-03 16:10:21 +0300

    - updated

--- pages/about/index.js
+++ pages/about/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Router from 'next/router'
 import {MainLayout} from "../../components/MainLayout";
 
-export default function Index() {
+export default function About( {title} ) {
 
     const linkClickHandler = () => {
         Router.push('/')
@@ -10,9 +10,13 @@ export default function Index() {
 
     return (
         <MainLayout title={'About Page'}>
-            <h1>About Page</h1>
+            <h1>{title}</h1>
             <button onClick={linkClickHandler}>Go back to home</button>
             <button onClick={() => Router.push('/posts')}>Go to posts</button>
         </MainLayout>
     )
+}
+
+About.getInitialProps = async () => {
+
 }
