11f7eeb93580f98b9a5b2a09454953c69163a611
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-03 15:58:50 +0300

    - updated

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,12 @@ import '../styles/main.scss'
 export default function MyApp({Component, pageProps}) {
     return (
         <>
+            <NextNprogress
+                color="#29D"
+                startPosition={0.3}
+                stopDelayMs={200}
+                height="3"
+            />
             <Component {...pageProps}/>
 {/*            <style jsx global>{`
             body {
