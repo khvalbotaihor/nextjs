ea20749f090341f8ab5ceb84e1537707580ba756
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-03 15:18:42 +0300

    - updated

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -11,7 +11,9 @@ export default function Post({post: serverPost}) {
             const data = await response.json();
             setPost(data)
         }
-        load()
+        if (!post){
+            load();
+        }
     }, [])
 
     return <MainLayout title={'Post page'}>
