92b6da08c80607451572960c58552dec04bd0413
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-03 09:35:44 +0300

    - updated

--- pages/posts.js
+++ pages/posts.js
@@ -18,7 +18,7 @@ export default function Posts() {
             <h1>Posts Page</h1>
             <p>lorem ipsum</p>
            <pre>
-               {JSON.stringify(posts)}
+               {JSON.stringify(posts, null, 2)}
            </pre>
         </MainLayout>
     )
