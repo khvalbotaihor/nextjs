641832cf5401b5dd68d2351ca68bbf0d551f19c8
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-05-08 13:34:50 +0300

    - updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Index() {
             <h1>Hello Next.JS!</h1>
             <p>lorem ipsum dolores</p>
             <p><Link href={'/about'}><a>About</a></Link></p>
-            <p><Link href='/posts'><a>Posts</a></Link></p>
+            <p><Link href={'/posts'}><a>Posts</a></Link></p>
         </MainLayout>
     )
 }
